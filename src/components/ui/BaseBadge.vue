7bdda30a8b7e0881a8f64d5310738fefa1e8cb2f
faisalhatila <hatilas1994@gmail.com>
2021-02-13 20:04:09 +0500

    base badge file createion done

--- src/components/ui/BaseBadge.vue
+++ src/components/ui/BaseBadge.vue
@@ -1,12 +1,17 @@
 <template>
-    <div>
-
-    </div>
+<span class="badge" :class="type">
+  {{text}}
+</span>
 </template>
 
 <script>
     export default {
-        
+     props:['type','title'],
+     computed:{
+       text(){
+         return this.title.toUpperCase();
+       }
+     }   
     }
 </script>
 
