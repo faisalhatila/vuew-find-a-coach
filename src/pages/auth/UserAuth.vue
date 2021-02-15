cf25b5f4355e1747555a778aff2bbe7c98d61a70
faisalhatila <hatilas1994@gmail.com>
2021-02-15 18:06:22 +0500

    auth page created

---
+++ src/pages/auth/UserAuth.vue
@@ -0,0 +1,64 @@
+<template>
+  <form>
+    <base-card>
+      <div class="form-contril">
+        <label for="email">E-Mail</label>
+        <input type="email" id="email" />
+      </div>
+      <div class="form-contril">
+        <label for="password">Password</label>
+        <input type="password" id="password" />
+      </div>
+      <base-button>Login</base-button>
+      <base-button type="button" mode="flat">Signup instead</base-button>
+    </base-card>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+form {
+  margin: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #ccc;
+  padding: 0.15rem;
+}
+
+input:focus,
+textarea:focus {
+  border-color: #3d008d;
+  background-color: #faf6ff;
+  outline: none;
+}
+
+.errors {
+  font-weight: bold;
+  color: red;
+}
+
+.actions {
+  text-align: center;
+}
+</style>
