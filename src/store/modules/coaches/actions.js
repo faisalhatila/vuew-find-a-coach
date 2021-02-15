437c42095d7497a3357cb36dee67ec14837a5b79
faisalhatila <hatilas1994@gmail.com>
2021-02-15 16:41:22 +0500

    transition animation added

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -28,7 +28,10 @@ export default {
       id: userId
     });
   },
-  async loadCoaches(context) {
+  async loadCoaches(context, payload) {
+    if (!payload.forceRefresh && !context.getters.shouldUpdate) {
+      return;
+    }
     const response = await fetch(
       `https://vue-http-demo-b10ca-default-rtdb.firebaseio.com/coaches.json`
     );
@@ -54,5 +57,6 @@ export default {
     }
 
     context.commit('setCoaches', coaches);
+    context.commit('setFetchTimestamp');
   }
 };
