47c9d8f6bacf115f5218bc8707b292149c416a87
faisalhatila <hatilas1994@gmail.com>
2021-02-16 17:14:58 +0500

    login module completed with firebase

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,8 +1,33 @@
 export default {
-  login() {},
-  async signup(context, payload) {
+  async login(context, payload) {
     const response = await fetch(
       `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBYA22ZdUr7kgTq4VErHJQWnon6jTzszfA`,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true
+        })
+      }
+    );
+    const responseData = response.json();
+
+    if (!response.ok) {
+      console.log(responseData);
+      const error = new Error(responseData.message || 'Failed to authenticate');
+      throw error;
+    }
+    console.log(responseData);
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn
+    });
+  },
+  async signup(context, payload) {
+    const response = await fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBYA22ZdUr7kgTq4VErHJQWnon6jTzszfA`,
       {
         method: 'POST',
         body: JSON.stringify({
